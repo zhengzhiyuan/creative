c75120ee9c706fc3167fbe3a977132d15f1f8533
zhengzhiyuan <>
2026-02-12 00:19:39 +0800

    加入配置

--- move_video/config.py
+++ move_video/config.py
@@ -12,6 +12,72 @@ class TaskType(Enum):
         "https://www.tiktok.com/@willstar.media",
         "/Users/huangyun/Desktop/搬运/A2"
     )
+    A3 = (
+        "A3",
+        "https://www.tiktok.com/@brai_sa",
+        "/Users/huangyun/Desktop/搬运/A3"
+    )
+    A4 = (
+        "A4",
+        "https://www.tiktok.com/@sasha.dumenko",
+        "/Users/huangyun/Desktop/搬运/A4"
+    )
+    A5 = (
+        "A5",
+        "https://www.tiktok.com/@ahmdhajri",
+        "/Users/huangyun/Desktop/搬运/A5"
+    )
+    A6 = (
+        "A6",
+        "https://www.tiktok.com/@cleaningwithida",
+        "/Users/huangyun/Desktop/搬运/A6"
+    )
+    A7 = (
+        "A7",
+        "https://www.tiktok.com/@danya.saunt",
+        "/Users/huangyun/Desktop/搬运/A7"
+    )
+    A8 = (
+        "A2",
+        "https://www.tiktok.com/@o_veronika_",
+        "/Users/huangyun/Desktop/搬运/A8"
+    )
+    A10 = (
+        "A10",
+        "https://www.tiktok.com/@_bitya_",
+        "/Users/huangyun/Desktop/搬运/A10"
+    )
+    A11 = (
+        "A11",
+        "https://www.tiktok.com/@migo0o0o07",
+        "/Users/huangyun/Desktop/搬运/A11"
+    )
+    A13 = (
+        "A13",
+        "https://www.tiktok.com/@xoteam",
+        "/Users/huangyun/Desktop/搬运/A13"
+    )
+    A17 = (
+        "A17",
+        "https://www.tiktok.com/@katy.vine",
+        "/Users/huangyun/Desktop/搬运/A17"
+    )
+    A18 = (
+        "A18",
+        "https://www.tiktok.com/@light_house_prod",
+        "/Users/huangyun/Desktop/搬运/A18"
+    )
+    A2 = (
+        "A2",
+        "https://www.tiktok.com/@willstar.media",
+        "/Users/huangyun/Desktop/搬运/A2"
+    )
+    A19 = (
+        "A19",
+        "https://www.tiktok.com/@natalikes",
+        "/Users/huangyun/Desktop/搬运/A19"
+    )
+
 
 # 副视频目录是全局统一的，保持不变
 GLOBAL_SUB_VIDEO_DIR = "/Users/huangyun/Desktop/搬运/副视频/data/关注/3710225754109904/视频"
